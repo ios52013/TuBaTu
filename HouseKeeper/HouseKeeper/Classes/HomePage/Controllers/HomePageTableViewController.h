9408ff022937e10e5007bbe3e5db95c104cec21e
tarena <2507233893@qq.com>
2016-09-30 14:41:05 +0800

    搭建基本框架、创建了首页的控制器

---
+++ HouseKeeper/HouseKeeper/Classes/HomePage/Controllers/HomePageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomePageTableViewController.h
+//  HouseKeeper
+//
+//  Created by aiqiyisheng on 16/9/30.
+//  Copyright © 2016年 黄永锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomePageTableViewController : UITableViewController
+
+@end
